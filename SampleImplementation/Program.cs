881de2f1e78ffa192450759cbd75bd4df4fef477
Christoffer Rosenqvist <christoffer.rosenqvist@invativa.se>
2014-03-22 15:50:47 +0100

    A class library and a sample implementation.

---
+++ SampleImplementation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Cros.SubRepoLib.SampleImplementation {
+    using System;
+    using Cros.SubRepoLib.Utilities;
+
+    public class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine(
+                General.SerializeJson(
+                    new {
+                        Foo = "Bar"
+                    }));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Utilities/General.cs
@@ -0,0 +1,9 @@
+﻿namespace Cros.SubRepoLib.Utilities {
+    using Newtonsoft.Json;
+
+    public class General {
+        public static string SerializeJson(object sourceObject) {
+            return JsonConvert.SerializeObject(sourceObject);
+        }   
+    }
+}
